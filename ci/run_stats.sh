6b8c7eff5387c87e927f19da4d5b9e6f54e52447
Edwin Cheng <edwin0cheng@gmail.com>
2019-11-23 16:28:38 +0800

    Add push to gp-pages

--- ci/run_stats.sh
+++ ci/run_stats.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# Make sure all permission are valid
+chmod -R a+rx ./ra_cli
+chmod a+rx ./ci/run_stats.sh
+chmod -R u+rw ./rustc-perf
+
+# ignore rustc-perf Cargo workspace
+if test -f ./rustc-perf/Cargo.toml; then
+    mv ./rustc-perf/Cargo.toml ./rustc-perf/Cargo.toml.bk
+fi
+
+# Variables
+
+RA_CLI=./ra_cli/ra_cli
+# COMMIT=$(cat ./ra_cli/commit)
+# OUTPUT_FILE="output-$COMMIT.txt"
+OUTPUT_FILE="ra-stats-output.txt"
+
+echo $(cat ./ra_cli/commit)
+echo "============="
+
 WHITE_LIST=(
     'cargo'
     'clap-rs'
@@ -21,13 +41,19 @@ is_white_listed () {
     done
 }
 
-# Assume ra_cli is in current dir
-DIRS=`ls -d ./rustc-perf/collector/benchmarks/*/`
+run_stats () {
+    # Assume ra_cli is in current dir
+    local DIRS=`ls -d ./rustc-perf/collector/benchmarks/*/`
 
-for D in $DIRS
-do
-    if [[ "$(is_white_listed $D)" == "True" ]]; then
-        echo "run analysis-stats on $D"
-        ./ra_cli/ra_cli analysis-stats $D
-    fi
-done
+    for D in $DIRS
+    do
+        if [[ "$(is_white_listed $D)" == "True" ]]; then
+            echo "run analysis-stats on $D"
+            echo "====START===="
+            $RA_CLI analysis-stats -q $D
+            echo "====END===="
+        fi
+    done
+}
+
+run_stats | tee $OUTPUT_FILE
