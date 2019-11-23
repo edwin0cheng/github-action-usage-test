c43dfe33b5023654919e0da4aa5b6dfba2524344
Edwin Cheng <edwin0cheng@gmail.com>
2019-11-23 15:26:18 +0800

    Add whitelist

--- ci/run_stats.sh
+++ ci/run_stats.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
+WHITE_LIST=(
+    'cargo'
+    'clap-rs'
+    'cranelift-codegen'
+    'futures'
+    'helloworld'
+    'html5ever' 
+    'syn'
+    'hyper'
+)
+
+is_white_listed () {
+    for F in ${WHITE_LIST[@]}
+    do
+        if [[ $1 == *"$F"* ]]; then
+            echo "True"
+            return 0
+        fi
+    done
+}
+
 # Assume ra_cli is in current dir
 DIRS=`ls -d ./rustc-perf/collector/benchmarks/*/`
 
 for D in $DIRS
 do
-    echo "run analysis-stats on $D"
-    ./ra_cli/ra_cli analysis-stats $D
+    if [[ "$(is_white_listed $D)" == "True" ]]; then
+        echo "run analysis-stats on $D"
+        ./ra_cli/ra_cli analysis-stats $D
+    fi
 done
