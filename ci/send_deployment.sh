446cc6e72e785ad9e97d68708c41e9faf22f3bb7
Edwin Cheng <edwin0cheng@gmail.com>
2019-11-25 03:06:47 +0800

    Another fix

--- ci/send_deployment.sh
+++ ci/send_deployment.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-curl -X POST -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.ant-man-preview+json" -H "Content-Type: application/json" "https://api.github.com/repos/$(GITHUB_REPOSITORY)/deployments" --data "{\"ref\": \"master\", \"required_contexts\": [], \"environment\": \"production\", \"payload\" : \"$(cat target_sha.txt)\" }"
+echo $GITHUB_REPOSITORY
+URL="https://api.github.com/repos/$GITHUB_REPOSITORY/deployments"
+
+curl -X POST -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.ant-man-preview+json" -H "Content-Type: application/json" $URL --data "{\"ref\": \"master\", \"required_contexts\": [], \"environment\": \"production\", \"payload\" : \"${cat target_sha.txt}\" }"
