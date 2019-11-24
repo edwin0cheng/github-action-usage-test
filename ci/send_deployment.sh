e019bebf2acb0b9365b09ea72ba03654a01b6e4b
Edwin Cheng <edwin0cheng@gmail.com>
2019-11-25 03:34:11 +0800

    Fix bad subst

--- ci/send_deployment.sh
+++ ci/send_deployment.sh
@@ -2,5 +2,5 @@
 
 echo $GITHUB_REPOSITORY
 URL="https://api.github.com/repos/$GITHUB_REPOSITORY/deployments"
-
-curl -X POST -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.ant-man-preview+json" -H "Content-Type: application/json" $URL --data "{\"ref\": \"master\", \"required_contexts\": [], \"environment\": \"production\", \"payload\" : \"${cat target_sha.txt}\" }"
+SHA=$(cat target_sha.txt)
+curl -X POST -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.ant-man-preview+json" -H "Content-Type: application/json" $URL --data "{\"ref\": \"master\", \"required_contexts\": [], \"environment\": \"production\", \"payload\" : \"$SHA\" }"
