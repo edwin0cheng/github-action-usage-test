d9eaadd38a7f1894e6029f4a41d6eb3106d5927d
Edwin Cheng <edwin0cheng@gmail.com>
2019-11-25 03:42:37 +0800

    Try to use personal token

--- ci/send_deployment.sh
+++ ci/send_deployment.sh
@@ -3,4 +3,4 @@
 echo $GITHUB_REPOSITORY
 URL="https://api.github.com/repos/$GITHUB_REPOSITORY/deployments"
 SHA=$(cat target_sha.txt)
-curl -X POST -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.ant-man-preview+json" -H "Content-Type: application/json" $URL --data "{\"ref\": \"master\", \"required_contexts\": [], \"environment\": \"production\", \"payload\" : \"$SHA\" }"
+curl -X POST -H "Authorization: token $TOKEN" -H "Accept: application/vnd.github.ant-man-preview+json" -H "Content-Type: application/json" $URL --data "{\"ref\": \"master\", \"required_contexts\": [], \"environment\": \"production\", \"payload\" : \"$SHA\" }"
