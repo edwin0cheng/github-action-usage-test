20c72ceaa1181ce00844a7f57338a09f5f9c3dad
Edwin Cheng <edwin0cheng@gmail.com>
2019-11-25 02:56:33 +0800

    Fix bug in issue and send_deployments

--- ci/send_deployment.sh
+++ ci/send_deployment.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X POST -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.ant-man-preview+json" -H "Content-Type: application/json" https://api.github.com/repos/$(GITHUB_REPOSITORY)/deployments --data "{\"ref\": \"master\", \"required_contexts\": [], \"environment\": \"production\", \"payload\" : \"$(cat target_sha.txt)\" }"
+curl -X POST -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.ant-man-preview+json" -H "Content-Type: application/json" "https://api.github.com/repos/$(GITHUB_REPOSITORY)/deployments" --data "{\"ref\": \"master\", \"required_contexts\": [], \"environment\": \"production\", \"payload\" : \"$(cat target_sha.txt)\" }"
