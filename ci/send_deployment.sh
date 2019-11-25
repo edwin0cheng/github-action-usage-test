253beb3c42f36bdec5ce2f5f2fc3504e20a52385
Edwin Cheng <edwin0cheng@gmail.com>
2019-11-26 01:03:03 +0800

    Fix payload quote

--- ci/send_deployment.sh
+++ ci/send_deployment.sh
@@ -6,6 +6,6 @@ SHA=$(cat target_sha.txt)
 ISSUE=$(cat issue.json)
 
 # We need quote the double quote as it is used in a quote later
-PAYLOAD="{ \"\"sha\"\" : \"\"$SHA\"\", \"\"issue\"\" : \"\"$ISSUE\"\" }"
+PAYLOAD="{ \\\"sha\\\" : \\\"$SHA\\\", \\\"issue\\\" : \\\"$ISSUE\\\" }"
 
 curl -X POST -H "Authorization: token $TOKEN" -H "Accept: application/vnd.github.ant-man-preview+json" -H "Content-Type: application/json" $URL --data "{\"ref\": \"master\", \"required_contexts\": [], \"environment\": \"production\", \"payload\" : \"$PAYLOAD\" }"
