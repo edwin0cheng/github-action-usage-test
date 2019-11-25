bf20b1dd45b254adfb4565c7622642a31b131303
Edwin Cheng <edwin0cheng@gmail.com>
2019-11-26 00:57:25 +0800

    Fix quote string

--- ci/send_deployment.sh
+++ ci/send_deployment.sh
@@ -4,4 +4,8 @@ echo $GITHUB_REPOSITORY
 URL="https://api.github.com/repos/$GITHUB_REPOSITORY/deployments"
 SHA=$(cat target_sha.txt)
 ISSUE=$(cat issue.json)
-curl -X POST -H "Authorization: token $TOKEN" -H "Accept: application/vnd.github.ant-man-preview+json" -H "Content-Type: application/json" $URL --data "{\"ref\": \"master\", \"required_contexts\": [], \"environment\": \"production\", \"payload\" : { \"sha\" : \"$SHA\", \"issue\" : \"$ISSUE\" } }"
+
+# We need quote the double quote as it is used in a quote later
+PAYLOAD="{ \"\"sha\"\" : \"\"$SHA\"\", \"\"issue\"\" : \"\"$ISSUE\"\" }"
+
+curl -X POST -H "Authorization: token $TOKEN" -H "Accept: application/vnd.github.ant-man-preview+json" -H "Content-Type: application/json" $URL --data "{\"ref\": \"master\", \"required_contexts\": [], \"environment\": \"production\", \"payload\" : \"$PAYLOAD\" }"
